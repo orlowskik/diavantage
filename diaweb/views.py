f4195a07d0f3ec19ce2999ddc5a27c14ed68be97
Kamil <orlowskik97@gmail.com>
2024-09-06 19:48:57 +0200

    Add Django REST framework and user API endpoints
    Incorporated Django REST framework into the project to enable API development. Added a user serializer for API representation and created user API endpoints using viewsets and routers. Modified `.gitignore`, `docker-compose.yml`, and settings configuration accordingly.

--- diaweb/views.py
+++ diaweb/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+
+from diaweb.serializers import UserSerializer
+
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
