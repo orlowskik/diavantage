a7e364db60bd01554c08b715a952cba15f97a2c2
Kamil <orlowskik97@gmail.com>
2024-09-13 14:01:46 +0200

    Add patient registration form and update related views, patient account creation
    Created a PatientRegistrationView to render this form using TemplateHTMLRenderer. After filling the form the POST request is send to PatientViewSet and validated data is saved to the database

--- diavantage/settings.py
+++ diavantage/settings.py
@@ -132,3 +132,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.TemplateHTMLRenderer',
+    ]
+}
--- diaweb/views.py
+++ diaweb/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
 from rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view, renderer_classes
 from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer, BrowsableAPIRenderer, HTMLFormRenderer
+from rest_framework.views import APIView
 
 from diaweb.models import Patient, Physician, Address, Glucose, Blood, Appointment, Reception
 from diaweb.serializers import PatientSerializer, PhysicianSerializer, AddressSerializer, \
@@ -16,40 +17,48 @@ from diaweb.serializers import PatientSerializer, PhysicianSerializer, AddressSe
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+
+class PatientRegistrationView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'diaweb/patient_registration.html'
+    style = {'template_pack': 'rest_framework/vertical'}
+    serializer = PatientSerializer
 
-    @action(detail=False, methods=['get'])
-    def get_creation_form(self, request):
-        return Response( {'serializer': self.get_serializer()}, template_name='diaweb/patient_registration.html',)
-
+    def get(self, request):
+        return Response({'serializer': self.serializer, 'style': self.style,
+                         'hidden_fields': ['id', 'confirmed_diabetes', 'classifier_result', 'last_appointment']})
 
 
 class PhysicianViewSet(viewsets.ModelViewSet):
     queryset = Physician.objects.all()
     serializer_class = PhysicianSerializer
 
+
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+
 
 class GlucoseViewSet(viewsets.ModelViewSet):
     queryset = Glucose.objects.all()
     serializer_class = GlucoseSerializer
 
+
 class BloodViewSet(viewsets.ModelViewSet):
     queryset = Blood.objects.all()
     serializer_class = BloodSerializer
 
+
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+
 
 class ReceptionViewSet(viewsets.ModelViewSet):
     queryset = Reception.objects.all()
     serializer_class = ReceptionSerializer
 
 
-
 class LoginPageView(TemplateView):
     template_name = 'diaweb/login.html'
-
