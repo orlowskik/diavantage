b3e3eaa98d4a87d3cdc64e1b9757564aea266241
orlow <orlowskik97@gmail.com>
2024-09-28 17:50:15 +0200

    Added data from Kaggle and created script for importing it to the database

--- diaweb/models.py
+++ diaweb/models.py
@@ -7,17 +7,17 @@ from django.utils import timezone
 from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError
 
-
 # Create your models here.
 User._meta.get_field('email')._unique = True
 
+
 class Address(models.Model):
-    country   = models.CharField(max_length=60)
-    state     = models.CharField(max_length=60)
-    city      = models.CharField(max_length=60)
-    zip_code   = models.CharField(max_length=10)
-    street    = models.CharField(max_length=100)
-    number    = models.CharField(max_length=10)
+    country = models.CharField(max_length=60)
+    state = models.CharField(max_length=60)
+    city = models.CharField(max_length=60)
+    zip_code = models.CharField(max_length=10)
+    street = models.CharField(max_length=100)
+    number = models.CharField(max_length=10)
     apartment = models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):
@@ -29,29 +29,28 @@ class Address(models.Model):
             'state': self.state,
             'city': self.city,
             'zip_code': self.zip_code,
-            'street' : self.street,
-            'number' : self.number,
-            'apartment' : self.apartment,
+            'street': self.street,
+            'number': self.number,
+            'apartment': self.apartment,
         }
 
         return data
 
 
 class Patient(models.Model):
-
     PREDICTIONS = {
-        -1 : "No Diabetes",
-        0  : "Not Performed",
-        1  : "Possible Diabetes"
+        -1: "No Diabetes",
+        0: "Not Performed",
+        1: "Possible Diabetes"
     }
 
-    user               = models.OneToOneField(User, on_delete=models.CASCADE)
-    birthdate          = models.DateField()
-    sex                = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthdate = models.DateField()
+    sex = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
     confirmed_diabetes = models.BooleanField(default=False)
-    classifier_result  = models.IntegerField(choices=PREDICTIONS, default=0)
-    last_appointment   = models.DateField(null=True, blank=True)
-    address            = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, blank=True)
+    classifier_result = models.IntegerField(choices=PREDICTIONS, default=0)
+    last_appointment = models.DateField(null=True, blank=True)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
@@ -70,15 +69,15 @@ class Patient(models.Model):
 
     def get_details(self):
         data = {
-            'name' : f'{self.user.first_name} {self.user.last_name}',
-            'email' : self.user.email,
-            'birthdate' : self.birthdate.strftime('%d-%m-%Y'),
-            'age'  : self.get_age(),
-            'sex'  : self.sex,
-            'address' : self.address.get_details() if self.address else None,
-            'diabetes' : self.confirmed_diabetes,
-            'prediction' : self.get_prediction(),
-            'last_appointment' : self.last_appointment
+            'name': f'{self.user.first_name} {self.user.last_name}',
+            'email': self.user.email,
+            'birthdate': self.birthdate.strftime('%d-%m-%Y'),
+            'age': self.get_age(),
+            'sex': self.sex,
+            'address': self.address.get_details() if self.address else None,
+            'diabetes': self.confirmed_diabetes,
+            'prediction': self.get_prediction(),
+            'last_appointment': self.last_appointment
         }
 
         return json.dumps(data, indent=4)
@@ -90,23 +89,23 @@ class Patient(models.Model):
             raise IntegrityError
         super().save(**kwargs)
 
-class Physician(models.Model):
 
-    user      = models.OneToOneField(User, on_delete=models.CASCADE)
+class Physician(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     specialty = models.CharField(max_length=60)
-    phone     = models.CharField(max_length=20)
-    patient   = models.ManyToManyField(Patient)
-    address   = models.ForeignKey(Address, on_delete=models.CASCADE, null=True, blank=True, unique=False)
+    phone = models.CharField(max_length=20)
+    patient = models.ManyToManyField(Patient)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True, blank=True, unique=False)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name} - {self.specialty}'
 
     def get_details(self):
         data = {
-            'name' : self.__str__(),
-            'email' : self.user.email,
-            'phone' : self.phone,
-            'address' : self.address.get_details() if self.address else None,
+            'name': self.__str__(),
+            'email': self.user.email,
+            'phone': self.phone,
+            'address': self.address.get_details() if self.address else None,
 
         }
 
@@ -117,10 +116,22 @@ class Physician(models.Model):
             raise IntegrityError
         super().save(**kwargs)
 
-class Glucose(models.Model):
 
-    patient          = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    measurement      = models.FloatField(validators=[MinValueValidator(0)])
+class Glucose(models.Model):
+    MEASUREMENT_TYPES = {
+        0: 'Undefined',
+        1: 'Before breakfast',
+        2: 'After breakfast',
+        3: 'Before lunch',
+        4: 'After lunch',
+        5: 'Before dinner',
+        6: 'After dinner'
+
+    }
+
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    measurement = models.FloatField(validators=[MinValueValidator(0)])
+    measurement_type = models.IntegerField(choices=MEASUREMENT_TYPES, default=0)
     measurement_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -137,19 +148,19 @@ class Glucose(models.Model):
                 raise ValidationError('Future date restricted')
         super().save(**kwargs)
 
+
 class Blood(models.Model):
-
-    patient             = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    systolic_pressure   = models.IntegerField(validators=[MinValueValidator(0)])
-    diastolic_pressure  = models.IntegerField(validators=[MinValueValidator(0)])
-    pulse_rate          = models.IntegerField(validators=[MinValueValidator(0)])
-    measurement_date    = models.DateTimeField(default=timezone.now)
-
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    systolic_pressure = models.IntegerField(validators=[MinValueValidator(0)])
+    diastolic_pressure = models.IntegerField(validators=[MinValueValidator(0)])
+    pulse_rate = models.IntegerField(validators=[MinValueValidator(0)])
+    measurement_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
         constraints = [
             models.CheckConstraint(check=models.Q(systolic_pressure__gte=0), name='Systolic_pressure_positive_number'),
-            models.CheckConstraint(check=models.Q(diastolic_pressure__gte=0), name='Diastolic_pressure_positive_number'),
+            models.CheckConstraint(check=models.Q(diastolic_pressure__gte=0),
+                                   name='Diastolic_pressure_positive_number'),
             models.CheckConstraint(check=models.Q(pulse_rate__gte=0), name='Pulse_rate_positive_number'),
         ]
 
@@ -159,30 +170,25 @@ class Blood(models.Model):
                 raise ValidationError('Future date restricted')
         super().save(**kwargs)
 
-class Appointment(models.Model):
 
-    patient   = models.ForeignKey(Patient, on_delete=models.CASCADE)
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     physician = models.ForeignKey(Physician, on_delete=models.SET_NULL, null=True)
-    date      = models.DateTimeField()
+    date = models.DateTimeField()
 
 
 class Reception(models.Model):
-
     DAYS = {
-        1 : "Monday",
-        2 : "Tuesday",
-        3 : "Wednesday",
-        4 : "Thursday",
-        5 : "Friday",
-        6 : "Saturday",
-        7 : "Sunday"
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+        7: "Sunday"
     }
 
     day = models.CharField(max_length=20, choices=DAYS)
     start_time = models.TimeField()
     end_time = models.TimeField()
     physician = models.ForeignKey(Physician, on_delete=models.SET_NULL, null=True, blank=True)
-
-
-
-
