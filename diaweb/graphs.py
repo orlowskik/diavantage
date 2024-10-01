aff9ff3c545efe39d2c5644fa473bdf6dd008ae2
Kamil <orlowskik97@gmail.com>
2024-10-01 17:15:04 +0200

    Created interactive statistical analysis of chosen data

--- diaweb/graphs.py
+++ diaweb/graphs.py
@@ -2,11 +2,13 @@ import time
 import dash
 import pandas as pd
 import plotly.express as px
-from dash import dcc, html, no_update, callback, callback_context
+from dash import dcc, html, dash_table ,no_update
 
 
 from django.utils import timezone
+from django.utils.termcolors import background
 from django_plotly_dash import DjangoDash
+from six import text_type
 
 from diaweb.models import Glucose, Blood
 
@@ -30,7 +32,7 @@ def get_marks(daterange, Nth=100):
 
     return result
 
-app = DjangoDash('SimpleExample')
+app = DjangoDash('MeasurementsAnalysis')
 
 app.layout = lambda: html.Div(children=[
     html.Fieldset(children=[
@@ -52,8 +54,84 @@ app.layout = lambda: html.Div(children=[
           html.Output(id='slider-output', children=[]),
         ])
     ]),
+    html.Div(id='data-statistics', children=[
+        html.Fieldset(children=[
+            html.Legend('Data Statistics'),
+            dash_table.DataTable(id='data-table',
+                                 columns=[{'name': i, 'id': i} for i in ['type', 'count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']],
+                                 data=[],
+                                 style_cell=dict(textAlign='center'),
+                                 style_header=dict(textAlign='center',
+                                                   text_type='italic',
+                                                   backgroundColor="cornflowerblue"),
+                                 style_as_list_view=True,
+                                 ),
+        ])
+    ]),
     html.Div(id='graph-content', children=[]),
 ])
+
+
+@app.callback(
+    dash.dependencies.Output('data-table', 'data'),
+    dash.dependencies.Input('date-slider', 'value'),
+    dash.dependencies.Input('graph-types', 'value'),
+)
+def update_data_statistics(slider_values, graph_types, *args, **kwargs):
+    if graph_types is None:
+        return no_update
+
+    patient_id = kwargs['request'].session['patient_id']
+
+    min_date = unix_to_datetime(slider_values[0])
+    max_date = unix_to_datetime(slider_values[1])
+
+    result = pd.DataFrame()
+
+    if 'Glucose' in graph_types:
+        data = Glucose.objects.filter(patient_id=patient_id, measurement_date__gte=min_date, measurement_date__lte=max_date).values('measurement',)
+        try:
+            df = pd.DataFrame(data).describe()
+            df = df._append(pd.Series({'measurement': 'GLU'}, name="type"))
+
+            result = result._append(df['measurement'])
+        except ValueError:
+            pass
+
+    if 'Sys' in graph_types:
+        data = Blood.objects.filter(measurement_date__gte=min_date, measurement_date__lte=max_date).values('systolic_pressure')
+        try:
+            df = pd.DataFrame(data).describe()
+            df = df._append(pd.Series({'systolic_pressure': 'SYS'}, name="type"))
+
+            result = result._append(df['systolic_pressure'])
+        except ValueError:
+            pass
+
+    if 'Dia' in graph_types:
+        data = Blood.objects.filter(measurement_date__gte=min_date, measurement_date__lte=max_date).values('diastolic_pressure')
+        try:
+            df = pd.DataFrame(data).describe()
+
+            df = df._append(pd.Series({'diastolic_pressure': 'DIA'}, name="type"))
+
+            result = result._append(df['diastolic_pressure'])
+        except ValueError:
+            pass
+
+
+    if 'Pulse' in graph_types:
+        data = Blood.objects.filter(measurement_date__gte=min_date, measurement_date__lte=max_date).values('pulse_rate')
+        try:
+            df = pd.DataFrame(data).describe()
+
+            df = df._append(pd.Series({'pulse_rate': 'PUL'}, name="type"))
+
+            result = result._append(df['pulse_rate'])
+        except ValueError:
+            pass
+
+    return result.to_dict('records')
 
 
 @app.callback(
@@ -85,25 +163,39 @@ def update_graph(graph_types, slider_values, *args, **kwargs):
     if 'Sys' in graph_types:
         data = Blood.objects.filter(patient_id=patient_id, measurement_date__gte=min_date, measurement_date__lte=max_date).values('systolic_pressure', 'measurement_date')
         df = pd.DataFrame(data)
-        figure = px.line(df, x='measurement_date', y='systolic_pressure')
+        try:
+            figure = px.line(df, x='measurement_date', y='systolic_pressure')
+        except ValueError:
+            figure = px.line()
 
         result.append(dcc.Graph(id='systolic-graph', figure=figure))
 
     if 'Dia' in graph_types:
         data = Blood.objects.filter(patient_id=patient_id, measurement_date__gte=min_date, measurement_date__lte=max_date).values('diastolic_pressure', 'measurement_date')
         df = pd.DataFrame(data)
-        figure = px.line(df, x='measurement_date', y='diastolic_pressure')
+
+        try:
+            figure = px.line(df, x='measurement_date', y='diastolic_pressure')
+        except ValueError:
+            figure = px.line()
 
         result.append(dcc.Graph(id='diastolic-graph', figure=figure))
 
     if 'Pulse' in graph_types:
         data = Blood.objects.filter(patient_id=patient_id, measurement_date__gte=min_date, measurement_date__lte=max_date).values('pulse_rate', 'measurement_date')
         df = pd.DataFrame(data)
-        figure = px.line(df, x='measurement_date', y='pulse_rate')
 
+        try:
+            figure = px.line(df, x='measurement_date', y='pulse_rate')
+        except ValueError:
+            figure = px.line()
         result.append(dcc.Graph(id='pulse-graph', figure=figure))
 
-    return result
+    return html.Fieldset(children=[
+        html.Legend('Graphs'),
+        html.Div(result)
+    ]) if len(result) > 0 else []
+
 @app.callback(
     dash.dependencies.Output('end-date', 'options'),
     dash.dependencies.Output('end-date', 'disabled'),
@@ -148,4 +240,5 @@ def update_slider_output(value, *args, **kwargs):
     if value is None:
         return no_update
 
-    return f'{unix_to_datetime(value[0])} - {unix_to_datetime(value[1])}'
+
+    return f'Start date: {unix_to_datetime(value[0])} / End date: {unix_to_datetime(value[1])}'
