08a6e0c38a0842e79a05597a48d66bca632de7a0
Kamil <orlowskik97@gmail.com>
2024-10-08 23:37:01 +0200

    Changed settings.py

--- diavantage/settings.py
+++ diavantage/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -149,3 +149,5 @@ REST_FRAMEWORK = {
 LOGIN_URL = '/api-auth/login/'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+CSRF_TRUSTED_ORIGINS=[os.environ.get('ALLOWED_HOSTS')]
